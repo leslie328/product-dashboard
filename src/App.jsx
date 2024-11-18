e6688830efe30171f191fa1426c564bba97c3981
Leslie Rodriguez <leslie286@usf.edu>
2024-11-17 22:13:52 -0500

    Set up App component with initial product list

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// code created to import information from files.
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [products, setProducts] = useState([
+    {id: 1, name: 'Sofa', price:400, description: 'unpholestered seat featuring aems and a back.'}
+    {id: 2, name: 'Fridge', price:800, description: 'a silver steel electronic refrigerator'}
+    {id: 3, name: 'Mirror', price:70, description: 'Made with premium materials, the frame ensures longevity and elegance'}
+  ]);
 }
+const addProduct = (newProduct) => {
+  set products([...products, newProduct]);
+};
+return (
+  <div>
+    <h1> Product Dashboard</h1>
+    <ProductList products={products} />
+  </div>
+)
+
 
 export default App
