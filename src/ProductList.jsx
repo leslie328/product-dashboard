111412b7b0697bf4b4f3377cb99947d6c534f713
Leslie Rodriguez <leslie286@usf.edu>
2024-11-17 22:32:07 -0500

    Create ProductList component to render each ProductItem

---
+++ src/ProductList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const products = [
+    { id: 1, name: 'Sofa', price: 400 },
+    { id: 2, name: 'Fridge', price: 800 },
+    { id: 3, name: 'Mirror', price: 70 },
+];
+
+function ProductList() {
+    return (
+        <div>
+            <h2>Product List</h2>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        {product.name} - ${product.price}
+                        key={product.id} 
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+export default ProductList;
